fef03692225cf870ee62c2f515ed011dc02fd665
vic-syryn <vic.syryn@ugent.be>
2026-03-15 16:20:52 +0100

    aanpassing project.ino

--- arduino/project/project.ino
+++ arduino/project/project.ino
@@ -7,9 +7,10 @@ void setup() {
 void loop() {
   for (int i = 0; i < 6; i++) {
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(100*14);
+    delay(100*12);
     digitalWrite(LED_BUILTIN, LOW);
-    delay(100*14);
+    delay(100*12);
   }
   Serial.println("Nog een rondje");
-  delay(100*14);
+  delay(100*12);
+}
